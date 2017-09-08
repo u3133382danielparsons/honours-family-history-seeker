d2bdba7b4b2736e3519964a958eab5bcdb2dc9d1
Daniel Parsons <danielparsons3133382@gmail.com>
2017-09-08 12:27:54 +1000

    Nav bar added

---
+++ src/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import App from './App.js';
+import Dashboard from './containers/Dashboard';
+import { Provider } from 'react-redux';
+import reducers from './reducers';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import { responsiveStoreEnhancer } from 'redux-responsive';
+import { createStore } from 'redux';
+import registerServiceWorker from './registerServiceWorker';
+
+const store = createStore(reducers, responsiveStoreEnhancer);
+injectTapEventPlugin();
+render(
+  <Provider store={store}>
+    <div>
+      <MuiThemeProvider>
+        <BrowserRouter>
+          <Switch>
+            <div>
+              <Route exact path="/" component={App} />
+              <Route path="/containers/Dashboard" component={Dashboard} />
+            </div>
+          </Switch>
+        </BrowserRouter>
+      </MuiThemeProvider>
+    </div>
+  </Provider>,
+  document.querySelector('#root')
+);
+
+registerServiceWorker();
---
+++ src/material-ui-responsive-drawer/store/selectors.js
@@ -0,0 +1,13 @@
+export const DEFAULT_BREAK_POINT = 'medium';
+
+export default function isResponsiveAndOverBreakPoint(
+  browser,
+  responsiveDrawer,
+  breakPoint
+) {
+  return (
+    browser.greaterThan[
+      breakPoint !== undefined ? breakPoint : DEFAULT_BREAK_POINT
+    ] && responsiveDrawer.responsive
+  );
+}
