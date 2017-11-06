0653af3f3115d3192cf7746cfa509163bfc95ef8
Daniel Parsons <danielparsons3133382@gmail.com>
2017-11-07 01:32:27 +1100

    Sign Up page form added

--- src/components/sign-up/SignUpMain.js
+++ src/components/sign-up/SignUpMain.js
@@ -1,26 +1,33 @@
 import React, { Component } from 'react';
-import { List, ListItem } from 'material-ui/List';
-import Subheader from 'material-ui/Subheader';
-import { Card, CardText } from 'material-ui/Card';
+import ReactSignupLoginComponent from './components/ReactSignupLoginComponent';
+import Paper from 'material-ui/Paper';
 import Divider from 'material-ui/Divider';
-import FlatButton from 'material-ui/FlatButton';
+import { Card } from 'material-ui/Card';
+import SocialLogin from './components/social-log.png';
+
+const style = {
+  height: 180,
+  width: 480,
+  padding: 20
+};
 
 class SignUpMain extends Component {
   render() {
     return (
       <main className="container">
         <div className="row">
+          <ReactSignupLoginComponent />
           <Divider />
-          <List>
-            <ListItem>
-              <Card>
-                <CardText>
-                  <Subheader>SignUp!</Subheader>
-                  <FlatButton />
-                </CardText>
-              </Card>
-            </ListItem>
-          </List>
+          <div className="social-login">
+            <Paper style={style} zDepth={5}>
+              <img src={SocialLogin} alt="Social Media Login Icons" />
+            </Paper>
+            <Card>
+              <h3 className="social-login-msg">
+                This is for Demonstration Purposes!
+              </h3>
+            </Card>
+          </div>
         </div>
       </main>
     );
